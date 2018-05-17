9d475862fc26743d733da39491fba553c8a642a2
Michael Neu <git@michaeln.eu>
2018-05-17 11:25:40 +0200

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist",
+  },
+
+  resolve: {
+    extensions: [
+      ".ts",
+      ".tsx",
+      ".js",
+      ".json",
+    ],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
