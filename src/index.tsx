431a43d01579bad8f4a6fb853acf9788de270ba5
Michael Neu <git@michaeln.eu>
2018-05-17 11:28:47 +0200

    add react

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,10 @@
-console.log("accommodate-react");
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const app = (
+  <h1>AccommoDate</h1>
+);
+
+const container = document.getElementById("app");
+
+ReactDOM.render(app, container);
