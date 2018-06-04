96fa1caadf928da324670651752cf4b7dafd83d3
Michael Neu <git@michaeln.eu>
2018-06-04 14:57:47 +0200

    remove unused import in reducers

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,5 @@
-import { Action } from "redux";
 import { AccommoDateState } from "../state";
 
-export const rootReducer = (state: AccommoDateState, action: Action) => {
+export const rootReducer = (state: AccommoDateState) => {
   return state;
 };
