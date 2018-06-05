5f2e9fad8c9d082faa7e8433ee44759cbe4d6ba8
Shoaib Khan <mshoaibkhan91@gmail.com>
2018-06-05 16:17:06 +0200

    unused param removed

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,5 @@
 import { AccommoDateState } from "../state";
 
 export const rootReducer = (state: AccommoDateState) => {
-  if (action) {console.log(action); } // temporary not to get noparam used error
   return state;
 };
