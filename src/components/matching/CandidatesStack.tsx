4d258ceea0f7795ddfd1ab8e26e6bdfee2f3b8b7
tomasruizt <tomas.ruiz.te@gmail.com>
2018-06-11 14:10:55 +0200

    Remove state placeholder

--- src/components/matching/CandidatesStack.tsx
+++ src/components/matching/CandidatesStack.tsx
@@ -1,10 +1,8 @@
 import * as React from "react";
 
-const CandidatesStack = () => (
+export const CandidatesStack = () => (
   <div>
     <h1>Candidates Stack</h1>
     <p>Candidate Stack placeholder</p>
   </div>
 );
-
-export { CandidatesStack };
--- src/components/matching/MatchingCockpit.tsx
+++ src/components/matching/MatchingCockpit.tsx
@@ -4,35 +4,17 @@ import { CandidatesStack } from "./CandidatesStack";
 import { MatchProps } from "./Match";
 import { MatchesList } from "./MatchesList";
 
-interface MatchingCockpitState {
+interface MatchingCockpitProps {
   matches: MatchProps[];
 }
 
-const MATCHES_PLACEHOLDER = {
-  matches: Array(5).fill(
-    {
-      chatStatusIcon: "Chatty",
-      matchIsStarred: true,
-      matchName: "Tomas",
-      matchPicture: "awesome",
-    } as MatchProps,
-  ),
-} as MatchingCockpitState;
-
-export class MatchingCockpit extends React.PureComponent<any, MatchingCockpitState> {
-
-  public state = MATCHES_PLACEHOLDER;
-
-  public render() {
-    return (
-      <Row>
-        <Col span={8}>
-          <MatchesList matches={this.state.matches} />
-        </Col>
-        <Col span={16}>
-          <CandidatesStack />
-        </Col>
-      </Row>
-    );
-  }
-}
+export const MatchingCockpit = (props: MatchingCockpitProps) => (
+  <Row>
+    <Col span={8}>
+      <MatchesList matches={props.matches} />
+    </Col>
+    <Col span={16}>
+      <CandidatesStack />
+    </Col>
+  </Row>
+);
