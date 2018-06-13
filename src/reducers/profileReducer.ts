db2bea8ab8a058e9851bb36c62ba37f4247e40d4
Michael Neu <git@michaeln.eu>
2018-06-13 15:13:30 +0200

    simplify profile reducer

--- src/reducers/profileReducer.ts
+++ src/reducers/profileReducer.ts
@@ -2,28 +2,20 @@ import { Action } from "../actions";
 import { ProfileActions } from "../actions/profileActions";
 import { ProfileState , UserMode } from "../components/profile/ProfileTypes";
 
-console.log("this is profile state and user mode", UserMode);
-
 const defaultState: ProfileState = {
   basicDetails: "",
   mode: UserMode.tenant,
 };
 
-export function ProfileReducer(state = defaultState, action: Action<UserMode>) {
+export const profileReducer = (state = defaultState, action: Action<UserMode>) => {
   switch (action.type) {
     case ProfileActions.SET_USER_MODE:
-    if (action.value === UserMode.tenant) {
       return {
         ...state,
-        mode: UserMode.tenant,
+        mode: action.value,
       };
-    } else {
-      return {
-        ...state,
-        mode: UserMode.landlord,
-      };
-    }
+
     default:
-    return state;
+      return state;
   }
-}
+};
