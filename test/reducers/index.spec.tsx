274691095ce697a81fedcb360598995cb72f95a9
Michael Neu <git@michaeln.eu>
2018-06-13 15:47:56 +0200

    merge root reducer test data to single object

--- test/reducers/index.spec.tsx
+++ test/reducers/index.spec.tsx
@@ -1,16 +1,14 @@
 import { rootReducer } from "../../src/reducers";
-import { RootState } from "../../src/state";
-import { Profile, UserMode } from "../../src/state/profile";
+import { AccommoDateState } from "../../src/state";
+import { UserMode } from "../../src/state/profile";
 
 describe("root reducer", () => {
   it("shouldn't alter the state if the action is unknown", () => {
-    const profileData: Profile = {
-      basicDetails : "",
-      mode : UserMode.Tenant,
-    };
-
-    const state: RootState = {
-      profile: profileData,
+    const state: AccommoDateState = {
+      profile: {
+        basicDetails : "",
+        mode : UserMode.Tenant,
+      },
     };
 
     const action = {
