c320dd9678d9402105dc0a585c3929aa27c91ffa
Michael Neu <git@michaeln.eu>
2018-06-16 18:17:36 +0200

    add user reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
-import { profileReducer } from "../reducers/profileReducer";
+import { profileReducer } from "./profileReducer";
+import { userReducer } from "./userReducer";
 
 export const rootReducer = combineReducers({
   profile : profileReducer,
+  user: userReducer,
 });
