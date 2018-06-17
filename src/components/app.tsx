a86dd201d58d8c44f2016c1aef86941b4019c9ac
aulona <aulonashabani@gmail.com>
2018-06-17 14:41:01 +0200

    for rent

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { TenantProfile } from "../components/create-profile/tenant-profile/TenantProfile";
+import { LandlordProfile } from "./create-profile/landlord-profile/LandlordProfile";
 import { MatchingCockpit } from "./matching/MatchingCockpit";
 
 export const AppComponent = () => (
@@ -7,6 +8,8 @@ export const AppComponent = () => (
     <h1>AccommoDate</h1>
     <MatchingCockpit matches={[]} />
     <TenantProfile />
+    <p>Landlord profile</p>
+    <LandlordProfile />
   </div>
 );
 
--- src/components/create-profile/landlord-profile/ForRent.tsx
+++ src/components/create-profile/landlord-profile/ForRent.tsx
@@ -1,8 +1,53 @@
+import { Icon, Modal, Upload } from "antd";
+import { UploadChangeParam, UploadFile } from "antd/lib/upload/interface";
 import * as React from "react";
 
-export const ForRent = () => (
-    <div>
-        <h1>For rent</h1>
-        <p>For rent</p>
-    </div>
-);
+export class ForRent extends React.Component {
+  public state = {
+    previewVisible: false,
+    previewImage: "",
+    fileList: [{
+      uid: -1,
+      name: "xxx.png",
+      status: "done",
+      url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    }],
+  };
+
+  public handleCancel = () => this.setState({ previewVisible: false });
+
+  public handlePreview = (file: UploadFile) => {
+    this.setState({
+      previewImage: file.url || file.thumbUrl,
+      previewVisible: true,
+    });
+  }
+
+  public handleChange = (fileList: UploadChangeParam) => this.setState(fileList);
+
+  public render() {
+    const { previewVisible, previewImage, fileList } = this.state;
+    const uploadButton = (
+      <div>
+        <Icon type="plus" />
+        <div className="ant-upload-text">Upload</div>
+      </div>
+    );
+    return (
+      <div className="forRentProperties">
+        <Upload
+          action="//jsonplaceholder.typicode.com/posts/"
+          listType="picture-card"
+          // fileList={fileList}
+          onPreview={this.handlePreview}
+          onChange={this.handleChange}
+        >
+          {fileList.length >= 3 ? null : uploadButton}
+        </Upload>
+        <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+          <img alt="example" style={{ width: "100%" }} src={previewImage} />
+        </Modal>
+      </div>
+    );
+  }
+}
