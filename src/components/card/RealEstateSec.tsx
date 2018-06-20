0b37bad3f2146adab2c4f22c070f4e81e154d762
Shoaib Khan <mshoaibkhan91@gmail.com>
2018-06-20 20:45:51 +0200

    grid horizontal division for realestates

--- src/components/card/RealEstateSec.tsx
+++ src/components/card/RealEstateSec.tsx
@@ -7,9 +7,27 @@ interface RealEstateProps {
 }
 
 export function RealEstateComponent(props: RealEstateProps) : JSX.Element {
+  const distArr: RealEstateType[][] = distProjects(props.realEstate);
   return (
     <Row>
-      <Col>{props.realEstate[0].address}</Col>
+      <Col span={24}>
+      {distArr.map((arr: RealEstateType[], index: number) => (<Row gutter={24} key={index}>{arr.map((item: RealEstateType, indx) => (<Col span={8} key={indx}>{item.name}</Col>))}</Row>))}
+      </Col>
     </Row>
   );
+}
+
+function distProjects(projectArr: RealEstateType[]) {
+  const gridArr: any = [];
+  for (let i = 1, j = 0  ; i <= projectArr.length ; i++) {
+    if (i % 3 === 0) {
+      if (gridArr[j] === undefined) { gridArr[j] = []; }
+      gridArr[j].push(projectArr[i - 1]);
+      j++;
+    } else {
+      if (gridArr[j] === undefined) { gridArr[j] = []; }
+      gridArr[j].push(projectArr[i - 1]);
+    }
+  }
+  return gridArr;
 }
