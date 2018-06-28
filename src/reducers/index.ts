c4e31d9f6f463dd5699fd73083f8090ccd914ec6
Shoaib Khan <mshoaibkhan91@gmail.com>
2018-06-28 22:30:52 +0200

    add editor to reducers

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import { biographyReducer } from "./biographyReducer";
+import { editorReducer } from "./editorReducer";
 import { loginReducer } from "./loginReducer";
 import { matchesReducer } from "./matchesReducer";
 import { signupReducer } from "./signupReducer";
@@ -7,6 +8,7 @@ import { storeReducer } from "./storeReducer";
 
 export const rootReducer = combineReducers({
   biography: biographyReducer,
+  editor: editorReducer,
   login: loginReducer,
   signup: signupReducer,
   store: storeReducer,
