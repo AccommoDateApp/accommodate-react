2135f78761878db7e8588de9730e8d36ba0c1ef7
aulonashabani <aulonashabani@gmail.com>
2018-06-29 18:57:27 +0200

    add action to upload accommodation images

--- src/actions/imagesAction.ts
+++ src/actions/imagesAction.ts
@@ -12,3 +12,13 @@ export const uploadProfileImage = (image: File) => {
     await finishSaving()(dispatch);
   };
 };
+
+export const uploadAccommodationImage = (accommodationId: string, image: File) => {
+  return async (dispatch: Dispatch) => {
+    dispatch(startSaving());
+    await api.uploadAccommodationImage(accommodationId, image);
+
+    await fetchBiography()(dispatch);
+    await finishSaving()(dispatch);
+  };
+};
