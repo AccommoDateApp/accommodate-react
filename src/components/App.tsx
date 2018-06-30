bf911b92075975dd9418885fc6a6f726a9dccabd
Michael Neu <git@michaeln.eu>
2018-06-30 11:08:55 +0200

    fetch bio at app start, if we're logged in already

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,45 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import { RouteComponentProps, withRouter } from "react-router";
+import { bindActionCreators, Dispatch } from "redux";
+import { fetchBiography } from "../actions/biographyActions";
 import { AccommoDateState, Fetchable } from "../state";
 import { Dashboard } from "./Dashboard";
 import { Landing } from "./landing/Landing";
 
 interface AppProps extends RouteComponentProps<any> {
   login: Fetchable<boolean>;
+  fetchBiography: typeof fetchBiography;
 }
 
-export const AppComponent = (props: AppProps) => {
-  if (props.login.value) {
-    return (
-      <Dashboard />
-    );
+export class AppComponent extends React.PureComponent<AppProps> {
+  public componentWillMount() {
+    if (this.props.login.value) {
+      this.props.fetchBiography();
+    }
   }
 
-  return (
-    <Landing />
-  );
-};
+  public render() {
+    if (this.props.login.value) {
+      return (
+        <Dashboard />
+      );
+    }
+
+    return (
+      <Landing />
+    );
+  }
+}
 
 const mapStateToProps = (state: AccommoDateState) => ({
   login: state.login,
 });
 
-export const App = withRouter(connect(mapStateToProps)(AppComponent));
+const mapDispatchToProps = (dispatch: Dispatch) => {
+  return bindActionCreators({
+    fetchBiography,
+  }, dispatch);
+};
+
+export const App = withRouter(connect(mapStateToProps, mapDispatchToProps)(AppComponent));
