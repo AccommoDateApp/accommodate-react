63cf4ee9c189d7310cf770b7937ce232587153c6
Michael Neu <git@michaeln.eu>
2018-06-30 11:15:57 +0200

    add test for fetching bio in App.spec.tsx

--- test/components/App.spec.tsx
+++ test/components/App.spec.tsx
@@ -1,5 +1,6 @@
 import { shallow } from "enzyme";
 import * as React from "react";
+import { spy } from "sinon";
 import { AppComponent } from "../../src/components/App";
 import { Dashboard } from "../../src/components/Dashboard";
 import { Landing } from "../../src/components/landing/Landing";
@@ -13,13 +14,19 @@ describe("AppComponent", () => {
     staticContext: null,
   };
 
+  let fetchBiography;
+
+  beforeEach(() => {
+    fetchBiography = spy();
+  });
+
   it("displays the landing page if the user is not logged in", () => {
     const login: Fetchable<boolean> = {
       ...defaultFetchableState,
       value: false,
     };
 
-    const rendered = shallow(<AppComponent {...routeProps} login={login} />);
+    const rendered = shallow(<AppComponent {...routeProps} login={login} fetchBiography={fetchBiography} />);
 
     expect(rendered.contains(<Landing />)).toBeTruthy();
   });
@@ -30,8 +37,18 @@ describe("AppComponent", () => {
       value: true,
     };
 
-    const rendered = shallow(<AppComponent {...routeProps} login={login} />);
+    const rendered = shallow(<AppComponent {...routeProps} login={login} fetchBiography={fetchBiography} />);
 
     expect(rendered.contains(<Dashboard />)).toBeTruthy();
+  });
+
+  it("fetches the biography on startup", () => {
+    const login: Fetchable<boolean> = {
+      ...defaultFetchableState,
+      value: true,
+    };
+
+    shallow(<AppComponent {...routeProps} login={login} fetchBiography={fetchBiography} />);
+    expect(fetchBiography.calledOnce).toBeTruthy();
   });
 });
